3e57cd8a91b79de90a65041d22feaee17d13f0d7
Natan <natanscosta7@gmail.com>
2026-07-09 22:02:42 -0300

    first hero profile

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "Natan";
+$class = "Guerreiro";
+$level = 1;
+$xpPoints = 0;
+
+echo "===== FICHA DO HERÓI =====", PHP_EOL;
+echo "Nome: $name", PHP_EOL;
+echo "Classe: $class", PHP_EOL;
+echo "Nível: $level", PHP_EOL;
+echo "XP: $xpPoints", PHP_EOL;
