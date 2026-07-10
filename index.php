ffe158e9ca56945949fa87cea82db20ea44ecfd8
Natan <natanscosta7@gmail.com>
2026-07-09 22:08:23 -0300

    hero profile using associative array

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
-$name = "Natan";
-$class = "Guerreiro";
-$level = 1;
-$xpPoints = 0;
+$hero = [
+  "name" => "Natan",
+  "class" => "Guerreiro",
+  "level" => 1,
+  "xpPoints" => 0,
+];
 
-echo "===== FICHA DO HERÓI =====", PHP_EOL;
-echo "Nome: $name", PHP_EOL;
-echo "Classe: $class", PHP_EOL;
-echo "Nível: $level", PHP_EOL;
-echo "XP: $xpPoints", PHP_EOL;
+echo "===== FICHA DO HERÓI =====" . PHP_EOL;
+echo PHP_EOL;
+echo "Nome: " . $hero["name"] . PHP_EOL;
+echo "Classe: " . $hero["class"] . PHP_EOL;
+echo "Nível: " . $hero["level"] . PHP_EOL;
+echo "XP: " . $hero["xpPoints"] . PHP_EOL;
