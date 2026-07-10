875fc5b3d3b5935919f425b1668ebd3233d0d835
Natan <natanscosta7@gmail.com>
2026-07-09 23:17:42 -0300

    save heroes using json file

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+function saveHeroes(array $heroes, string $path): void
+{
+    file_put_contents($path, json_encode($heroes, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
+
+function loadHeroes(string $path): array
+{
+    return json_decode(file_get_contents($path), true);
+}
+
 function getHeroTitle(int $xp): string
 {
     if ($xp < 100) {
