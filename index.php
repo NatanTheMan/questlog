17c75d91e5213e970cb8c3e406ababd87bd2a1f0
Natan <natanscosta7@gmail.com>
2026-07-09 22:31:46 -0300

    implementes first functions

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
 
+
+function printHero(array $hero): void
+{
+  echo "===== FICHA DO HERÓI =====" . PHP_EOL;
+  echo PHP_EOL;
+  echo "Nome: " . $hero["name"] . PHP_EOL;
+  echo "Classe: " . $hero["class"] . PHP_EOL;
+  echo "Nível: " . $hero["level"] . PHP_EOL;
+  echo "XP: " . $hero["xp"] . PHP_EOL;
+  echo "Título: " . $hero["title"] . PHP_EOL;
+}
+
+function getHeroTitle(int $xp): string
+{
+  if ($xp < 100) {
+    return "Novato";
+  } elseif ($xp < 500) {
+    return "Aventureiro";
+  } elseif ($xp < 1000) {
+    return "Veterano";
+  } else {
+    return "Lendário";
+  }
+}
+
+
 $hero = [
   "name" => "Natan",
   "class" => "Guerreiro",
   "level" => 1,
-  "xpPoints" => 0,
+  "xp" => 40,
+  "title" => "",
 ];
 
-echo "===== FICHA DO HERÓI =====" . PHP_EOL;
-echo PHP_EOL;
-echo "Nome: " . $hero["name"] . PHP_EOL;
-echo "Classe: " . $hero["class"] . PHP_EOL;
-echo "Nível: " . $hero["level"] . PHP_EOL;
-echo "XP: " . $hero["xpPoints"] . PHP_EOL;
+$hero["title"] = getHeroTitle($hero["xp"]);
+printHero($hero);
